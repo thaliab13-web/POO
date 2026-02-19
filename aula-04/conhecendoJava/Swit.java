e53bb88f17c1c74a3c2a54fae32233477270291f
thaliab13-web <thalia.b13@aluno.ifsc.edu.br>
2026-02-19 20:14:55 -0300

    Adicionando um arquivo .java

---
+++ aula-04/conhecendoJava/Swit.java
@@ -0,0 +1,17 @@
+void main() {
+     
+    String s;
+
+    int i = 0;
+
+    switch (i) {
+        case 1:
+            s = "Um";
+            break;
+        default:
+            s = "Outro";
+            break;
+
+    }
+    IO.println(s);
+}
