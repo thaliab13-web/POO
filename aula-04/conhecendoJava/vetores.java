cf5f9384943ee514790b0ff184528383e677fe25
thaliab13-web <thaliab13@aluno.ifsc.edu.br>
2026-02-23 12:30:44 -0300

    Adiciona exemplos de vetor em Java

---
+++ aula-04/conhecendoJava/vetores.java
@@ -0,0 +1,28 @@
+public class vetores {
+
+    public static void main(String[] args) {
+        
+        // Vetor de inteiros com 5 posições
+
+        int[] numeros = new int[5];
+        numeros[0] = 10; // primeira posição é 0
+        numeros[4] = 20; // última posição é 4
+
+        //inicialização direta
+        int[] pares = {2, 4, 6, 8}; // ou int[] impares = new int[]{1, 3, 5, 7};
+        
+        // Percorrer todos os elementos de um vetor
+
+        int[] numero = {10, 20 ,30};
+
+        for (int i = 0; i < numero.length; i++) {
+            System.out.println(numero[i]);   
+        }
+
+        System.out.println("=======================");
+        // for-each
+        for(int valor : numero) {
+            System.out.println(valor);
+        }
+    }
+}
