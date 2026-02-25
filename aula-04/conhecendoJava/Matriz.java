ce903254e036f5bd0fc7bf87d6084c4cf2886869
thaliab13-web <thaliab13@aluno.ifsc.edu.br>
2026-02-24 21:16:13 -0300

    Atualiza Matriz com for-each e exercicios

--- aula-04/conhecendoJava/Matriz.java
+++ aula-04/conhecendoJava/Matriz.java
@@ -1,28 +1,42 @@
 public class Matriz {
 
     public static void main(String[] args) {
-        
-        // Percorrer todos os elementos de uma matriz usando dois laços for ou for-each aninhados
-        
+            
+
         int[][] matriz = {
             {1, 2, 3},
-            {4, 5, 6}
+            {4, 5, 6},
+            {7, 8, 9}
         };
-        // for tradicional
-
-        for(int i = 0; i < matriz.length; i++) {
-            for(int j = 0; j < matriz.length; j++) {
-                System.out.println(matriz[i][j]);
+        System.out.println("Matriz 3x3");
+        System.out.println();
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+        
+                System.out.print(matriz[i][j] + "\t");
             }
+            System.out.println();
         }
-        System.out.println("========================");
+        System.out.println("====================");
+        System.out.println("Diagonal Principal");
 
-        // Usando for-each
+        for(int j = 0; j < 3; j++) {
+            // Acessa matriz[0][0], matriz[1][1], matriz[2][2]
 
+            System.out.println(matriz[j][j] + " ");
+        }   
+        System.out.println("====================");
+
+
+        
+        System.out.println("Usando for-each"); 
         for(int[] linha : matriz) {
             for(int valor : linha) {
-                System.out.println(valor);
+                System.out.print(valor + "\t");
             }
+            System.out.println();
         }
+    
     }
 }
+
