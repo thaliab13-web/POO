dad792c2c410efd2672ee31f20b1ecfb773ceebf
thaliab13-web <thaliab13@aluno.ifsc.edu.br>
2026-02-24 21:16:55 -0300

    Atualiza Vetor com for-each e exercicios

--- aula-04/conhecendoJava/Vetores.java
+++ aula-04/conhecendoJava/Vetores.java
@@ -1,28 +1,24 @@
 public class Vetores {
 
     public static void main(String[] args) {
-        
-        // Vetor de inteiros com 5 posições
 
         int[] numeros = new int[5];
         numeros[0] = 10; // primeira posição é 0
         numeros[4] = 20; // última posição é 4
 
-        //inicialização direta
-        int[] pares = {2, 4, 6, 8}; // ou int[] impares = new int[]{1, 3, 5, 7};
-        
-        // Percorrer todos os elementos de um vetor
+        int[] impares = new int[10];
+        impares[0] = 3;
 
-        int[] numero = {10, 20 ,30};
-
-        for (int i = 0; i < numero.length; i++) {
-            System.out.println(numero[i]);   
+        for (int i = 1; i < impares.length; i++) {
+            impares[i] = impares[i - 1] + 2;
+            System.out.println(impares[i]);
         }
 
         System.out.println("=======================");
-        // for-each
-        for(int valor : numero) {
-            System.out.println(valor);
+        System.out.println("Usando for-each");
+        for (int valor : numeros) {
+            System.out.print(valor + "\t");
         }
+
     }
 }
