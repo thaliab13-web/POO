faedf68b9afa811f3c6526449466644486fc89ed
thaliab13-web <thaliab13@aluno.ifsc.edu.br>
2026-03-10 15:21:43 -0300

    Adiciona código ao exercicio

--- src/main/java/lab02/MatrizComAsteriscos.java
+++ src/main/java/lab02/MatrizComAsteriscos.java
@@ -1,4 +1,15 @@
 package lab02;
 
 public class MatrizComAsteriscos {
+    static void main(String[] args) {
+
+        int tamanho = 9;
+        char[][] matriz = new char[tamanho][tamanho];
+
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = 0; j < tamanho; j++) {
+                matriz[i][j] = '*';
+            }
+        }
+    }
 }
