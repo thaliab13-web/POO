617699d24ab180f19f6ba97b512879509f89e8f8
thaliab13-web <thaliab13@aluno.ifsc.edu.br>
2026-04-17 21:28:13 -0300

    Atualiza código

--- src/main/java/aula16/Aviao.java
+++ src/main/java/aula16/Aviao.java
@@ -82,6 +82,7 @@ public class Aviao {
 
     // método ligar motor individualmente
     public void ligarMotor(int posicao) {
+        int indice = posicao - 1; // converte 1 -> 0
             if(posicao >= 0 && posicao <= 8) {
                motores.get(posicao).ligar();
            } else {
@@ -91,7 +92,8 @@ public class Aviao {
 
     // método desligar motor individualmente
     public void desligarMotor(int posicao) {
-            if (posicao >= 0 && posicao <= 8) {
+        int indice = posicao - 1; // converte 1 -> 0
+        if (posicao >= 0 && posicao <= 8) {
                 motores.get(posicao).desligar();
             } else {
                 IO.println("ERRO, NÚMERO DE MOTOR INVÁLIDO");
@@ -102,7 +104,7 @@ public class Aviao {
     // método ver estado do motor (ligado ou desligado)
     public void verStatus() {
         for (int i = 0; i < motores.size(); i++) {
-            IO.println("Motor n° " + i + "status: " + motores.get(i).getEstadoAtual());
+            IO.println("Motor n° " + (i + 1) + " - status: " + motores.get(i).getEstadoAtual());
         }
     }
 
--- src/main/java/aula16/Main.java
+++ src/main/java/aula16/Main.java
@@ -5,26 +5,40 @@ public class Main {
     static void main(String[] args) {
 
 
-        Aviao aviao = new Aviao(2, 10, 15,"turbina", 4);
+        Aviao aviao = new Aviao(2, 10, 15,"turbina", 8);
 
 
+        IO.println("...Inicialização...");
         IO.println("Quantidade de Motores: "  + aviao.getMotores().size());
         aviao.verStatus();
         IO.println();
 
-        IO.println("Ligando todos os motores");
+        IO.println("Ligando Avião (todos os motores)");
         aviao.ligar();
         aviao.verStatus();
         IO.println();
 
-        IO.println("Desligando todos os motores");
+        IO.println("Desligando Avião (todos os motores)");
         aviao.desligar();
         aviao.verStatus();
         IO.println();
 
-        IO.println("Ligar o motor 2 ");
+        IO.println("Ligar o motor individual (posição 3) ");
         aviao.ligarMotor(2);
         aviao.verStatus();
+        IO.println();
+
+        aviao.desligar();
+
+
+        IO.println("Ligar o motor individual (posição 8) ");
+        aviao.ligarMotor(7);
+        aviao.verStatus();
+        IO.println();
+
+        IO.println("Teste de erro (motor inválido) ");
+        aviao.ligarMotor(10);
+        IO.println();
 
 
 
