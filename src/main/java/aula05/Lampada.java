2a3beb8e52ece3ed3e63005ff74e4085c82c5827
thaliab13-web <thaliab13@aluno.ifsc.edu.br>
2026-04-17 21:10:21 -0300

    Atualiza código

--- src/main/java/aula05/Lampada.java
+++ src/main/java/aula05/Lampada.java
@@ -18,29 +18,6 @@ public class Lampada {
 
 
 
-    public void ligar() {
-        if (!ligado) {
-            ligado = true;
-            System.out.println("Motor ligado com sucesso.");
-        } else {
-            System.out.println("O motor já está ligado.");
-        }
-    }
 
-    // Método para desligar o motor
-    public void desligar() {
-        if (ligado) {
-            ligado = false;
-            System.out.println("Motor desligado.");
-        } else {
-            System.out.println("O motor já está desligado.");
-        }
-    }
-
-    // Verifica o estado atual
-    public boolean isLigado() {
-        return ligado;
-    }
-}
 
 }
